6c44f5d363713d3f5bcc110a1669a7fa17bb50e0
Denis Craig <admin@deniscraig.com>
2025-04-20 19:59:19 +0100

    Update flake-nix-fmt to 0.0.2

--- flake.nix
+++ flake.nix
@@ -2,11 +2,29 @@
   description = "Nix utility library";
 
   inputs = {
-    flake-schemas.url = "https://flakehub.com/f/DeterminateSystems/flake-schemas/*";
-    flake-utils.url = "https://flakehub.com/f/numtide/flake-utils/0.1.*";
-    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.2411.*";
+    flake-schemas = {
+      type = "github";
+      owner = "DeterminateSystems";
+      repo = "flake-schemas";
+      ref = "refs/tags/v0.1.5";
+    };
+    flake-utils = {
+      type = "github";
+      owner = "numtide";
+      repo = "flake-utils";
+      ref = "refs/tags/v1.0.0";
+    };
+    nixpkgs = {
+      type = "github";
+      owner = "NixOS";
+      repo = "nixpkgs";
+      ref = "24.11";
+    };
     flake-nix-fmt = {
-      url = "github:Denis101/flake-nix-fmt/0.0.1";
+      type = "github";
+      owner = "Denis101";
+      repo = "flake-nix-fmt";
+      ref = "0.0.2";
       inputs.flake-schemas.follows = "flake-schemas";
       inputs.flake-utils.follows = "flake-utils";
       inputs.nixpkgs.follows = "nixpkgs";
