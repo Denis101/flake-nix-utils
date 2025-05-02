30190f7d1f4f6540de34783ff9c12a1847463da5
Denis Craig <admin@deniscraig.com>
2025-05-02 18:47:55 +0100

    Apply nix formatting fixes

--- flake.nix
+++ flake.nix
@@ -33,112 +33,113 @@
     flake-utils,
     nix-fmt,
     ...
-  } @ inputs: rec {
-    schemas = flake-schemas.schemas;
-    formatter = nix-fmt.formatter;
-    lib = import ./lib.nix inputs;
+  } @ inputs:
+    rec {
+      schemas = flake-schemas.schemas;
+      formatter = nix-fmt.formatter;
+      lib = import ./lib.nix inputs;
 
-    tests = {
-      testLinuxSystems = {
-        expr = lib.linuxSystems;
-        expected = ["aarch64-linux" "i686-linux" "x86_64-linux"];
-      };
+      tests = {
+        testLinuxSystems = {
+          expr = lib.linuxSystems;
+          expected = ["aarch64-linux" "i686-linux" "x86_64-linux"];
+        };
 
-      testDarwinSystems = {
-        expr = lib.darwinSystems;
-        expected = ["aarch64-darwin" "x86_64-darwin"];
-      };
+        testDarwinSystems = {
+          expr = lib.darwinSystems;
+          expected = ["aarch64-darwin" "x86_64-darwin"];
+        };
 
-      testDefaultFilesInDir = {
-        expr = builtins.length (lib.defaultFilesInDir "${self}/testData");
-        expected = 2;
-      };
+        testDefaultFilesInDir = {
+          expr = builtins.length (lib.defaultFilesInDir "${self}/testData");
+          expected = 2;
+        };
 
-      testDataFileAttrset = {
-        expr = lib.fileAttrset "${self}/testData/default.nix";
-        expected = {
-          name = "testData";
-          value = { a = "test"; };
+        testDataFileAttrset = {
+          expr = lib.fileAttrset "${self}/testData/default.nix";
+          expected = {
+            name = "testData";
+            value = {a = "test";};
+          };
+        };
+
+        testNestedFileAttrset = {
+          expr = lib.fileAttrset "${self}/testData/nested/default.nix";
+          expected = {
+            name = "nested";
+            value = {a = "nest";};
+          };
+        };
+
+        testFlattenAttrs = {
+          expr = lib.flattenAttrset {
+            foo = {baz = {a = 1;};};
+            bar = {bah = {b = 1;};};
+          };
+          expected = {
+            baz = {a = 1;};
+            bah = {b = 1;};
+          };
+        };
+
+        testFlattenAttrsOverwrite = {
+          expr = lib.flattenAttrset {
+            foo = {baz = {a = 1;};};
+            bar = {baz = {b = 2;};};
+          };
+          expected = {
+            baz = {a = 1;};
+          };
+        };
+
+        testFlattenAttrsOverwriteReverse = {
+          expr = lib.flattenAttrset {
+            foo = {baz = {b = 2;};};
+            bar = {baz = {a = 1;};};
+          };
+          expected = {
+            baz = {b = 2;};
+          };
+        };
+
+        testForAllSystems = {
+          expr = lib.forAllSystems (system: {a = system;});
+          expected = {
+            aarch64-darwin = {a = "aarch64-darwin";};
+            aarch64-linux = {a = "aarch64-linux";};
+            i686-linux = {a = "i686-linux";};
+            x86_64-darwin = {a = "x86_64-darwin";};
+            x86_64-linux = {a = "x86_64-linux";};
+          };
         };
       };
-
-      testNestedFileAttrset = {
-        expr = lib.fileAttrset "${self}/testData/nested/default.nix";
-        expected = {
-          name = "nested";
-          value = { a = "nest"; };
-        };
+    }
+    // flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      checks = {
+        fmt = nix-fmt.checks.${system}.fmt;
+        # test = pkgs.stdenvNoCC.mkDerivation {
+        #   name = "test";
+        #   src = ./.;
+        #   dontBuild = true;
+        #   doCheck = true;
+        #   nativeBuildInputs = with pkgs; [nix-unit];
+        #   checkPhase = ''
+        #     nix-unit --impure --extra-experimental-features flakes --flake ${self}#tests
+        #   '';
+        #   installPhase = "mkdir \"$out\"";
+        # };
       };
 
-      testFlattenAttrs = {
-        expr = lib.flattenAttrset {
-          foo = { baz = { a = 1; }; };
-          bar = { bah = { b = 1; }; };
+      devShells = {
+        default = pkgs.mkShellNoCC {
+          packages = with pkgs; [alejandra nix-unit];
         };
-        expected = {
-          baz = { a = 1; };
-          bah = { b = 1; };
-        };
-      };
 
-      testFlattenAttrsOverwrite = {
-        expr = lib.flattenAttrset {
-          foo = { baz = { a = 1; }; };
-          bar = { baz = { b = 2; }; };
-        };
-        expected = {
-          baz = { a = 1; };
+        githubActions = pkgs.mkShellNoCC {
+          packages = with pkgs; [j2cli nix-unit];
         };
       };
-
-      testFlattenAttrsOverwriteReverse = {
-        expr = lib.flattenAttrset {
-          foo = { baz = { b = 2; }; };
-          bar = { baz = { a = 1; }; };
-        };
-        expected = {
-          baz = { b = 2; };
-        };
-      };
-
-      testForAllSystems = {
-        expr = lib.forAllSystems (system: { a = system; });
-        expected = {
-          aarch64-darwin = { a = "aarch64-darwin"; };
-          aarch64-linux = { a = "aarch64-linux"; };
-          i686-linux = { a = "i686-linux"; };
-          x86_64-darwin = { a = "x86_64-darwin"; };
-          x86_64-linux = { a = "x86_64-linux"; };
-        };
-      };
-    };
-  }
-  // flake-utils.lib.eachDefaultSystem (system: let
-    pkgs = import nixpkgs {inherit system;};
-  in {
-    checks = {
-      fmt = nix-fmt.checks.${system}.fmt;
-      # test = pkgs.stdenvNoCC.mkDerivation {
-      #   name = "test";
-      #   src = ./.;
-      #   dontBuild = true;
-      #   doCheck = true;
-      #   nativeBuildInputs = with pkgs; [nix-unit];
-      #   checkPhase = ''
-      #     nix-unit --impure --extra-experimental-features flakes --flake ${self}#tests
-      #   '';
-      #   installPhase = "mkdir \"$out\"";
-      # };
-    };
-
-    devShells = {
-      default = pkgs.mkShellNoCC {
-        packages = with pkgs; [alejandra nix-unit];
-      };
-
-      githubActions = pkgs.mkShellNoCC {
-        packages = with pkgs; [j2cli nix-unit];
-      };
-    };
-  });
+    });
 }
--- lib.nix
+++ lib.nix
@@ -2,37 +2,37 @@ inputs: let
   nixpkgs = inputs.nixpkgs.lib;
   flake-utils = inputs.flake-utils.lib;
 in rec {
-    linuxSystems = builtins.filter (nixpkgs.hasSuffix "linux") flake-utils.defaultSystems;
-    darwinSystems = builtins.filter (nixpkgs.hasSuffix "darwin") flake-utils.defaultSystems;
+  linuxSystems = builtins.filter (nixpkgs.hasSuffix "linux") flake-utils.defaultSystems;
+  darwinSystems = builtins.filter (nixpkgs.hasSuffix "darwin") flake-utils.defaultSystems;
 
-    defaultFilesInDir = directory:
-      nixpkgs.pipe (nixpkgs.filesystem.listFilesRecursive directory) [
-        (builtins.filter (name: nixpkgs.hasSuffix "default.nix" name))
-      ];
+  defaultFilesInDir = directory:
+    nixpkgs.pipe (nixpkgs.filesystem.listFilesRecursive directory) [
+      (builtins.filter (name: nixpkgs.hasSuffix "default.nix" name))
+    ];
 
-    fileAttrset = file: {
-      name = builtins.baseNameOf (builtins.dirOf file);
-      value = import file;
-    };
+  fileAttrset = file: {
+    name = builtins.baseNameOf (builtins.dirOf file);
+    value = import file;
+  };
 
-    defaultFilesAttrset = directory:
-      nixpkgs.pipe (defaultFilesInDir directory) [
-        (map (file: {
-          name = builtins.baseNameOf (builtins.dirOf file);
-          value = import file;
-        }))
-        (builtins.listToAttrs)
-      ];
+  defaultFilesAttrset = directory:
+    nixpkgs.pipe (defaultFilesInDir directory) [
+      (map (file: {
+        name = builtins.baseNameOf (builtins.dirOf file);
+        value = import file;
+      }))
+      (builtins.listToAttrs)
+    ];
 
-    flattenAttrset = attrs: builtins.foldl' nixpkgs.mergeAttrs {} (builtins.attrValues attrs);
+  flattenAttrset = attrs: builtins.foldl' nixpkgs.mergeAttrs {} (builtins.attrValues attrs);
 
-    forAllSystems = nixpkgs.genAttrs flake-utils.defaultSystems;
+  forAllSystems = nixpkgs.genAttrs flake-utils.defaultSystems;
 
-    pkgsBySystem = forAllSystems (
-      system:
-        import inputs.nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-        }
-    );
-  }
+  pkgsBySystem = forAllSystems (
+    system:
+      import inputs.nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      }
+  );
+}
